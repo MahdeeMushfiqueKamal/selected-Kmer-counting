04f47b3a07004c6acb05d9850c7acae11e314686
mmk <mmkdhrubo@gmail.com>
2022-09-06 04:40:53 +0600

    fixed issue

--- read_kmer_testing.cpp
+++ read_kmer_testing.cpp
@@ -1,18 +1,12 @@
-// g++ read_kmer.cpp -o a && ./a
 #include <iostream>
 #include<bits/stdc++.h>
 #include <fstream>
 using namespace std;
 
-//unsigned long long MASK = 1ull<<45ull -1;
-//MASK = MASK << 14;
 uint64_t MASK = 0b11111111111111111111111111111111111111111111;
-string current_kmer_str ="";
-
-ofstream outfile("all_kmers_from_c.txt");
 
 // for testing purpose
-map<int,int> m; int true_count; string kmerStr;
+map<uint64_t,int> m; int true_count; string kmerStr;
 
 uint64_t cal(string str_k_mer)
 {
@@ -41,8 +35,6 @@ uint64_t cal(string str_k_mer)
     }
     return k_mer;
 }
-
-
 
 
 int64_t addChar(int64_t k_mer, char ch)
@@ -110,32 +102,8 @@ void read_kmer(bool canonicalize) {
     int chunk_size = 1000;
     char arr_chunk[chunk_size];
     int kmerLen = 22;
-    int occurance = 0;
 
     bool isInHeader = false;
-
-//    char sample1[]="GATGATTCCATTTGATTCCATT";
-//    char sample2[]="AATGGAATCAAATGGAATCATC";
-//    char sample3[]="ATGAATGGAATCGAATGGAATC";
-//    int64_t sample_kmer1 = cal(sample1);
-//    int64_t sample_kmer2 = cal(sample2);
-//    int64_t sample_kmer3 = cal(sample3);
-
-//
-//    int64_t mask_1, mask_2;
-//        if(2*kmerLen>30){
-//            mask_1 = (1 << 30);
-//            mask_1 = ~(mask_1 << (2*(kmerLen-1)-30));
-//            mask_2 = (1 << 30);
-//            mask_2 = ~(mask_2 << ((2*(kmerLen-1)-30)+1));
-//            //cout<<"inside if"<<endl;
-//
-//        }
-//        else{
-//            mask_1 = ~(1<<(2*(kmerLen-1)));
-//            mask_2 = ~(1<<((2*(kmerLen-1))+1));
-//            //cout<<"inside else"<<endl;
-//        }
 
     int chunk_count = 0;
     int kmer_count = 0;
@@ -148,25 +116,18 @@ void read_kmer(bool canonicalize) {
 
         while(i<chunk_size)
         {
-            if(arr_chunk[i]==EOF)
-                break;
-
+            if(arr_chunk[i]==EOF)break;
             char ch = arr_chunk[i];
             if (ch == '\n')line_no++;
-//            cout<<"char found "<<ch<<" "<<isInHeader<<endl;
             if(ch == '>')
             {
                 isInHeader = true;
                 currentKmer = 0;
                 current_len = 0;
-                current_kmer_str = "";
             }
             else if (isInHeader==true && ch=='\n')
             {
                 isInHeader = false;
-                currentKmer = 0;
-                current_len = 0;
-                current_kmer_str = "";
             }
 
             if(isInHeader)
@@ -184,7 +145,6 @@ void read_kmer(bool canonicalize) {
             {
                 currentKmer = 0;
                 current_len = 0;
-                current_kmer_str = "";
                 i+=1;
                 continue;
             }
@@ -195,36 +155,16 @@ void read_kmer(bool canonicalize) {
                 {
                     currentKmer = addChar(currentKmer, ch) & MASK;
                     current_len++;
-                    current_kmer_str += ch;
-//                    cout<<current_kmer_str<<endl;
                 }
                 else
                 {
-//                    currentKmer = currentKmer & mask_1;
-//                    currentKmer = currentKmer & mask_2;
-                    currentKmer = addChar(currentKmer, ch);
-                    currentKmer &= MASK;
-                    //current_len++;
-                    current_kmer_str = current_kmer_str.substr(1,21) + ch;
-//                    cout<<current_kmer_str<<endl;
+                    currentKmer = addChar(currentKmer, ch) & MASK;
                 }
                 //cout<<currentKmer<<endl;
 
                 if(current_len==22)
                 {
-//                    outfile<<current_kmer_str<<endl;
-                    if (current_kmer_str == "TTCAAAGCTGCTCTGTAAAAAG" || current_kmer_str =="CTTTTTACAGAGCAGCTTTGAA")occurance++;
-                    if (current_kmer_str != "CTTTTTACAGAGCAGCTTTGAA" && currentKmer == cal("CTTTTTACAGAGCAGCTTTGAA")) cout<<"same value for int found "<<current_kmer_str<<endl;
-                    if(currentKmer != cal(current_kmer_str)){
-                        cout<<"DISCRIpency found for "<<currentKmer<<"---------------\n";
-                        cout<<"Currentkmer is: "<<currentKmer<<endl;
-                        cout<<"kmer string is: "<<current_kmer_str<<" "<<current_kmer_str.length()<<endl;
-                        cout<<"cal value is: "<<cal(current_kmer_str)<<endl;
-                        cout<<"line_no is: "<<line_no<<endl;
-                    }
-
                     //GOT KMER -- do the necessary things
-                    //if(canonicalize) reverse_compliment(currentKmer, kmerLen);
                     auto it = m.find(currentKmer);
                     if(it != m.end()) {
                         // key exist in map. so increase
@@ -239,22 +179,19 @@ void read_kmer(bool canonicalize) {
             }
             i+=1;
         }
-//        if(chunk_count == 100)break;
     }
-    cout<<occurance<<endl;
     //cout<<chunk_count<<endl;
 }
 
 
 int main() {
-    cout<<"value of MASK: "<<MASK<<endl;
-    ifstream infile("selected_true_count.txt");
+//    cout<<"value of MASK: "<<MASK<<endl;
+    ifstream infile("test_exact_count_lhg22L20MC5x_20000.txt");
     while(infile>>kmerStr>>true_count){
         m[cal(kmerStr)] = 0;   // for all kmer in test_file setting the value one;
     }
     infile.close();
-    //cout<<cal("AATGGAATCAAATGGAATCATC")<<endl;
-    //cout<<cal("ATGAATGGAATCGAATGGAATC")<<endl;
+
     read_kmer(true);
 
     ifstream infile2("selected_true_count.txt");
